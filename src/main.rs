e60e6194c8be6f999b4abb027556a9dbebd60af1
julien deslandes <julien.deslandes@azuria.earth>
2025-02-19 11:38:13 +0100

    add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,102 @@
-use axum::{routing::post, Router};
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p example-tracing-aka-logging
+//! ```
 
-mod handlers;
+use std::time::Duration;
+
+use axum::{
+    body::Bytes,
+    extract::MatchedPath,
+    http::{HeaderMap, Request},
+    response::{Html, Response},
+    routing::get,
+    Router,
+};
+
+use tokio::{net::TcpListener, time::sleep};
+use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
+use tracing::{info_span, Span};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new()
-        .route("/", post(handlers::echo))
-        .route("/users", post(handlers::create_user));
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                format!(
+                    "{}=debug,tower_http=info,axum::rejection=trace",
+                    env!("CARGO_CRATE_NAME")
+                )
+                .into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(handler))
+        // `TraceLayer` is provided by tower-http so you have to add that as a dependency.
+        // It provides good defaults but is also very customizable.
+        //
+        // See https://docs.rs/tower-http/0.1.1/tower_http/trace/index.html for more details.
+        //
+        // If you want to customize the behavior using closures here is how.
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    // Log the matched route's path (with placeholders not filled in).
+                    // Use request.uri() or OriginalUri if you want the real path.
+                    let matched_path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        matched_path,
+                        testa = "cool",
+                        status = tracing::field::Empty,
+                    )
+                })
+                .on_request(|_request: &Request<_>, _span: &Span| {
+                    // You can use `_span.record("some_other_field", value)` in one of these
+                    // closures to attach a value to the initially empty field in the info_span
+                    // created above.
+                    tracing::info!("{} {}", _request.method(), _request.uri().path())
+                })
+                .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
+                    _span.record("status", _response.status().as_str());
+                    tracing::debug!("response {} generated in {:?}", _response.status(), _latency)
+                })
+                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
+                    // ...
+                })
+                .on_eos(
+                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
+                        // ...
+                    },
+                )
+                .on_failure(
+                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+                        tracing::debug!("error {} generated in {:?}", _error, _latency)
+                    },
+                ),
+        );
+
+    // run it
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    tracing::debug!("Hello mike");
+    sleep(Duration::from_millis(3000)).await;
+    Html("<h1>Hello, World!</h1>")
 }
